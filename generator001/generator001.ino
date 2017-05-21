7ba5e43a1c3ed1ee868cbb9ecc521e624fa93bc4
Sergey Bolotov <bessarab27@gmail.com>
2017-05-21 18:25:21 +0400

    Initial commit

---
+++ generator001/generator001.ino
@@ -0,0 +1,13 @@
+byte OUTPUT_PIN = 9;
+
+void setup() {
+	// 10 бит 1 562,5 Гц
+	TCCR1A = TCCR1A & 0xe0 | 3;
+	TCCR1B = TCCR1B & 0xe0 | 0x09;
+}
+
+void loop() {
+	for (int i=0; i < 1024; i++){ 
+		analogWrite(OUTPUT_PIN, i); delay(100);
+	}
+}
